c4d44b2f32955a11eba114a143c98e25cdb8a73b
WillChew <williamchew95@gmail.com>
2018-06-04 15:52:33 -0400

    part 1 finished

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,42 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+       
+        // 255 unit long array of characters
+        char inputChars[255];
+        char number[255];
+        int operation = 0;
+        
+        while (number != nil) {
+        
+        printf("Input a string: ");
+        // limit input to max 255 characters
+        fgets(inputChars, 255, stdin);
+        NSLog(@"input a number (1-6): ");
+        
+        
+        fgets(number, 255, stdin);
+        operation = atoi(number);
+        while (operation > 6) {
+            NSLog(@"Invalid number, input number from 1-6");
+                fgets(number, 6, stdin);
+                operation = atoi(number);
+            }
+        
+        
+        
+        // print as a c string
+        printf("Your string is %s\n", inputChars);
+        NSLog(@"Your number is %d", operation);
+        
+        // convert char array to an NSString object
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        
+        // print NSString object
+        NSLog(@"Input was: %@", inputString);
+        }
     }
     return 0;
 }
