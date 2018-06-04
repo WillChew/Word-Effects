8c9e12138bb59f53b173dbec3f4a78a916483e3a
WillChew <williamchew95@gmail.com>
2018-06-04 15:10:37 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Will Chew on 2018-06-04.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
